bfed1d87c1197e462e715401d0b061cf300c6e53
semsun <sunyongqian@masget.com>
2017-10-20 14:20:39 +0800

    First commit

---
+++ src/main/java/com/semsun/web/weixin/TestController.java
@@ -0,0 +1,28 @@
+package com.semsun.web.weixin;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.PrintWriter;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+	@RequestMapping(value="/hello",method={RequestMethod.POST,RequestMethod.GET})
+	public void hello( @RequestParam(value="data",required=false) String data, HttpServletRequest request, HttpServletResponse response) {
+		try {
+			response.setCharacterEncoding("UTF-8");
+			PrintWriter out = response.getWriter();
+			out.write( String.format("Hello World! [%s]", data) );
+			out.flush();
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
